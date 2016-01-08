69fbcd694d688044597f2f18a253a881405a1570
kfmauric <kyle.maurice@gmail.com>
2016-01-08 13:38:52 -0800

    almost complete need to debug

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -10,7 +10,71 @@ def dprint(dstr):
 
 
 def hw(sent_fp, tweet_fp):
-    dprint('Hello, world!')
+    #dprint('Hello, world!')
+    states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+    }
+
+    states_count = {}
+    inv_state = {v: k for k, v in states.items()}
+    #print inv_state
+    state_scores = {}
     scores = {} # initialize an empty dictionary
     for line in sent_fp:
         dprint(line)
@@ -25,18 +89,51 @@ def hw(sent_fp, tweet_fp):
         dprint(line)
         dprint(type(line))
         json_line = json.loads(line)
-        #json_line = json_line.encode('ascii', 'ignore')
+        # json_line = json_line.encode('ascii', 'ignore')
         dprint(json_line)
         if ('text' in json_line):
             dprint(json_line['text'].encode('ascii', 'ignore'))
-            for word in json_line['text'].split((' ')):
+            for word in json_line['text'].split(' '):
                 dprint(word.encode('ascii', 'ignore'))
                 dprint('--------------------')
-                if(word in scores.keys() and word != ""):
+                if (word in scores.keys()):
                     line_score = line_score + scores[word]
         else: dprint('no text')
-        print
-        print(line_score)
+        if ('place' in json_line):
+           if (json_line['place']):
+               if(json_line['place']['country_code']=="US"):
+                    # print json_line['place']['full_name']
+                    a,b = json_line['place']['full_name'].split(',')
+                    # print a
+                    # print b
+                    if (b.lstrip(' ') == 'USA'):
+                        cur_state = inv_state[a]
+                     #   print cur_state,
+                     #   print 'foo'
+                    elif b.lstrip(' ') in states.keys():
+                        cur_state = b.lstrip(' ')
+                      #  print cur_state,
+                      #  print 'boo'
+                    else:
+                        cur_state = 'null'
+                       # print cur_state,
+                       # print 'roo'
+                    if cur_state in state_scores.keys():
+                        state_scores[cur_state]['score'] += line_score
+                        state_scores[cur_state]['tweets'] += 1
+                    else:
+                        state_scores[cur_state] = {'score':line_score, 'tweets':1}
+    max_score=0
+    max_state='null'
+    for state in state_scores.keys():
+        cur_score = float(state_scores[state]['score'])/state_scores[state]['tweets']
+        if max_score <= cur_score:
+            max_score = cur_score
+            max_state = state
+    print max_state,
+    print max_score
+
+
 
 
 def lines(fp):
@@ -46,8 +143,6 @@ def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     hw(sent_file, tweet_file)
-    #lines(sent_file)
-    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -3,49 +3,50 @@ import json
 import uu
 
 
+import sys
+import json
+import uu
+
+
 def dprint(dstr):
     debug_flag = 0
     if (debug_flag):
         print(dstr)
 
 
-def hw(sent_fp, tweet_fp):
-    dprint('Hello, world!')
-    scores = {} # initialize an empty dictionary
-    for line in sent_fp:
-        dprint(line)
-        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-        scores[term] = int(score)  # Convert the score to an integer.
-
-    dprint(scores.items()) # Print every (term, score) pair in the dictionary
-
-
+def hw(tweet_fp):
+    htag_scores = {}
     for line in tweet_fp:
-        line_score = 0
         dprint(line)
         dprint(type(line))
         json_line = json.loads(line)
         #json_line = json_line.encode('ascii', 'ignore')
-        dprint(json_line)
-        if ('text' in json_line):
-            dprint(json_line['text'].encode('ascii', 'ignore'))
-            for word in json_line['text'].split((' ')):
-                dprint(word.encode('ascii', 'ignore'))
-                dprint('--------------------')
-                if(word in scores.keys() and word != ""):
-                    line_score = line_score + scores[word]
-        else: dprint('no text')
-        print
-        print(line_score)
+        if ('entities' in json_line):
+            #print(json_line['entities'])
+            if ('hashtags' in json_line['entities']):
+                if json_line['entities']['hashtags']:
+                    #print(json_line['entities']['hashtags'])
+                    for elem in json_line['entities']['hashtags']:
+                        temp=dict(elem)
+                        #print temp['text'].encode('ascii', 'ignore')
+                        if temp not in htag_scores.keys():
+                            htag_scores[temp['text']]=1
+                        #print new_scores
+                        else:
+                            htag_scores[temp['test']] += 1
+    for word in htag_scores.keys():
+        print word.encode('ascii', errors='replace'),
+        print htag_scores[word]
+        #else: dprint('no text')
+        #print(line_score)
 
 
 def lines(fp):
     print(str(len(fp.readlines())))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw(sent_file, tweet_file)
+    tweet_file = open(sys.argv[1])
+    hw(tweet_file)
     #lines(sent_file)
     #lines(tweet_file)
 
